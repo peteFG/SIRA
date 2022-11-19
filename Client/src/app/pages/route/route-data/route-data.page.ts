7947da6a93b9ac7de0414abc175b0ca1c55e990d
Marcel Puchas <marcel.puchas@dedalus.eu>
2022-11-19 15:44:10 +0100

    add button to route data page

--- Client/src/app/pages/route/route-data/route-data.page.ts
+++ Client/src/app/pages/route/route-data/route-data.page.ts
@@ -14,8 +14,29 @@ export interface DataCheckState {
 })
 export class RouteDataPage implements OnInit {
   public dataCheckValues = dataCheckValues;
+  public isAllChecked: boolean = false;
+  public buttonText: string = "Ohne Auswahl fortfahren";
   constructor() {}
 
 
   ngOnInit() {}
+
+  public onAllStateChanged() {
+    if(this.isAllChecked)
+      this.dataCheckValues.forEach(val => val.state = true);
+      this.setButtonText();
+    }
+
+  public onCheckboxStateChanged() {
+    this.isAllChecked = this.dataCheckValues.filter(val => !val.state).length === 0;
+    this.setButtonText();
+  }
+
+  public setButtonText() {
+    this.buttonText = this.dataCheckValues.filter(val => val.state).length > 0 ? "Daten übermitteln": "Ohne Auswahl fortfahren";
+  }
+
+  public onSendData() {
+    
+  }
 }
